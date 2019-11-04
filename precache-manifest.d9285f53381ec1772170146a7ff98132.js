55fb273b04290d6d677f6c1e8aa0939f3ed75ee5
wisam450 <44022407+wisam450@users.noreply.github.com>
2019-11-04 22:17:25 +0100

    Updates

---
+++ precache-manifest.d9285f53381ec1772170146a7ff98132.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f8710166bc29a0b55f1a0bc2573baf",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "1defb1a52f3882913564",
+    "url": "/todo/static/css/main.b067866a.chunk.css"
+  },
+  {
+    "revision": "b150ae01874231adcaea",
+    "url": "/todo/static/js/2.c60fa162.chunk.js"
+  },
+  {
+    "revision": "1defb1a52f3882913564",
+    "url": "/todo/static/js/main.d7f9ed66.chunk.js"
+  },
+  {
+    "revision": "d2e524ea1f27d29a2a6a",
+    "url": "/todo/static/js/runtime-main.ff5f1930.js"
+  }
+]);
